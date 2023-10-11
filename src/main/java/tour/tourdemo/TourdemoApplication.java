c301b105910ede790275ad472e8498ac77de0b07
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-10-11 11:48:44 +0200

    second commit

---
+++ src/main/java/tour/tourdemo/TourdemoApplication.java
@@ -0,0 +1,13 @@
+package tour.tourdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tour/tourdemo/TourdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tour.tourdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
