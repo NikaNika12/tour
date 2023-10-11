e4c5e30a99152ec01d8134a879e6c2b3fc400aed
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-10-11 22:49:05 +0200

    fix problem with ApplicationController

--- src/main/java/tour/tourdemo/controller/ApplicationController.java
+++ src/main/java/tour/tourdemo/controller/ApplicationController.java
@@ -1,22 +1,31 @@
 package tour.tourdemo.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tour.tourdemo.model.Application;
 import tour.tourdemo.service.ApplicationService;
-import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/app")
 @RestController
+@RequestMapping("/application")
 public class ApplicationController {
+    @Autowired
     private final ApplicationService applicationService;
-    @GetMapping("/all")
-    public List<Application> getAllApp() {
+
+    @GetMapping("/findAll")
+    public List<Application> getAll() {
         return applicationService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @PostMapping("/save")
     public Application save(@RequestBody Application application) {
         return applicationService.save(application);
     }
