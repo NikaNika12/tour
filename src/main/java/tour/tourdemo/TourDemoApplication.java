852cba27fa1d0c4b3b40c6bb649a643f311721ca
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-10-19 11:36:49 +0200

    commit and push for Leonid Sivko

--- src/main/java/tour/tourdemo/TourDemoApplication.java
+++ src/main/java/tour/tourdemo/TourDemoApplication.java
@@ -2,6 +2,7 @@ package tour.tourdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TourDemoApplication {
--- src/main/java/tour/tourdemo/controller/TourController.java
+++ src/main/java/tour/tourdemo/controller/TourController.java
@@ -1,7 +1,14 @@
 package tour.tourdemo.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tour.tourdemo.dto.request.TourRequestDto;
 import tour.tourdemo.dto.response.TourResponseDto;
 import tour.tourdemo.mapper.TourMapper;
