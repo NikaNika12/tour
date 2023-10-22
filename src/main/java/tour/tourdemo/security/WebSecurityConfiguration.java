dc48104a21fc966ae597a5c1c53de3452798ccba
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-10-22 21:56:47 +0200

    completed security

--- src/main/java/tour/tourdemo/security/WebSecurityConfiguration.java
+++ src/main/java/tour/tourdemo/security/WebSecurityConfiguration.java
@@ -3,10 +3,12 @@ package tour.tourdemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,16 +34,17 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults())
-                .csrf(Customizer.withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/tours/findAll")
-                                .permitAll()
-                                .anyRequest()
+                                .requestMatchers("/tours/**")
                                 .authenticated()
+                                .anyRequest()
+                                .permitAll()
                 )
-                .formLogin(withDefaults())
+//                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
                 .userDetailsService(userDetailsService)
                 .build();
     }
