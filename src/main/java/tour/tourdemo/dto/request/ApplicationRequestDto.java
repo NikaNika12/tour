56ba4326631cf29057ef7f18a6c9f760172ce646
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-10-24 13:12:54 +0200

    add "NotNull"

--- src/main/java/tour/tourdemo/dto/request/ApplicationRequestDto.java
+++ src/main/java/tour/tourdemo/dto/request/ApplicationRequestDto.java
@@ -1,10 +1,13 @@
 package tour.tourdemo.dto.request;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 public class ApplicationRequestDto {
+    @NotNull
     private String fullName;
+    @NotNull
     private String contact;
     private boolean isDeleted;
 }
--- src/main/java/tour/tourdemo/dto/request/TourRequestDto.java
+++ src/main/java/tour/tourdemo/dto/request/TourRequestDto.java
@@ -1,14 +1,21 @@
 package tour.tourdemo.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TourRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String description;
+    @NotNull
     private String date;
+    @NotNull
     private Integer price;
+    @NotNull
     private String location;
+    @NotNull
     private String type;
     private boolean isDeleted;
 }
